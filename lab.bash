37e9e8a44fb6a8d02d5b7414762ab537f762400d
HPPinata <83947761+HPPinata@users.noreply.github.com>
2022-04-16 21:20:07 +0200

    Update lab.bash

--- lab.bash
+++ lab.bash
@@ -6,16 +6,16 @@ function main {
 }
 
 function loop {
-	if (($COUNTER == 10)); then exit; fi;
+	if (($COUNTER = 10)); then exit; fi;
 
 	BACK=$RANDOM % 2
 
-	if (($BACK == 0)) && (($(pwd) == C:\Users\Moritz\Documents\Test)); then loop; else cont; fi;
+	if (($BACK = 0)) && (($(pwd) == C:\Users\Moritz\Documents\Test)); then loop; else cont; fi;
 }
 
 function cont {
-	if (($BACK == 0)); then cd ..; fi;
-	if (($BACK == 1)); then check; fi;
+	if (($BACK = 0)); then cd ..; fi;
+	if (($BACK = 1)); then check; fi;
 	loop
 }
 
@@ -25,10 +25,10 @@ function check {
 
 function random {
 	GO=$RANDOM % 4
-	if (($GO == 0)); then cd rechts; fi
-	if (($GO == 1)); then cd links; fi
-	if (($GO == 2)); then cd oben; fi
-	if (($GO == 3)); then cd unten; fi
+	if (($GO = 0)); then cd rechts; fi
+	if (($GO = 1)); then cd links; fi
+	if (($GO = 2)); then cd oben; fi
+	if (($GO = 3)); then cd unten; fi
 	loop
 }
 
