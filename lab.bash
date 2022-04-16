a4255dc1af6144a611ffe2f22a2e09c00b88c68a
HPPinata <83947761+HPPinata@users.noreply.github.com>
2022-04-16 21:34:54 +0200

    Update lab.bash

--- lab.bash
+++ lab.bash
@@ -14,8 +14,8 @@ function loop {
 }
 
 function cont {
-	if (( $BACK = 0 )); then cd ..; fi;
-	if (( $BACK = 1 )); then check; fi;
+	if (( $BACK == 0 )); then cd ..; fi;
+	if (( $BACK == 1 )); then check; fi;
 	loop
 }
 
@@ -25,10 +25,10 @@ function check {
 
 function random {
 	GO=$RANDOM % 4
-	if (( $GO = 0 )); then cd rechts; fi
-	if (( $GO = 1 )); then cd links; fi
-	if (( $GO = 2 )); then cd oben; fi
-	if (( $GO = 3 )); then cd unten; fi
+	if (( $GO == 0 )); then cd rechts; fi
+	if (( $GO == 1 )); then cd links; fi
+	if (( $GO == 2 )); then cd oben; fi
+	if (( $GO == 3 )); then cd unten; fi
 	loop
 }
 
