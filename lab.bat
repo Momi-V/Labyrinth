6f666cd5867e67722805bc2d680d2efa3c8c405e
HPPinata <83947761+HPPinata@users.noreply.github.com>
2022-04-17 10:56:10 +0200

    Update lab.bat

--- lab.bat
+++ lab.bat
@@ -15,7 +15,8 @@ if %BACK% equ 1 cd ..
 goto :loop
 
 :check
-dir /b | >nul findstr "^" && (goto :random) || (goto :create)
+dir /b | >nul findstr "^" && (call :random) || (call :create)
+goto :loop
 
 :random
 set /A GO=%random% %% 4
@@ -23,7 +24,6 @@ if %GO% equ 0 cd rechts
 if %GO% equ 1 cd links
 if %GO% equ 2 cd oben
 if %GO% equ 3 cd unten
-goto :loop
 
 :create
 set /A COUNTER=COUNTER+1
@@ -31,8 +31,8 @@ mkdir rechts
 mkdir links
 mkdir oben
 mkdir unten
-goto :loop
 
 :link
+call :random
 type nul >"cloudnextcloud.dynv6.net s nFFg8fELscHofm4"
 EXIT
